68b0e8d67b1c2ca569046f5a63d4091478bf675d
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-17 04:41:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,5 +15,7 @@ class CashRegister
       @to_take_off = (price * discount)/100
       @total -= @to_take_off
       return "After the discount, the total comes to #{total}."
+    else
+      return ""
   end
 end
