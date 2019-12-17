edf5c6e41705a9bca18142eda127022260d01a29
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-17 04:21:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :discount, :price, :items, :last_transaction
-  def initialize(total)
-    @total = zero
+  def initialize(discount = 0)
+    @discount = zero
   end
   def new
     @new
